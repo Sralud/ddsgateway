8643141970a81d66391b82134f7f5d60ba3c4423
Sralud <llorente.gerald2@gmail.com>
2025-04-11 14:21:00 +0100

    update for deployment

--- config/services.php
+++ config/services.php
@@ -2,12 +2,12 @@
 
 return [
     'users1' => [
-        'base_uri' => env('USER1_SERVICE_BASE_URL', 'http://localhost:8000/'),
+        'base_uri' => env('USERS1_SERVICE_BASE_URL', 'https://lumen-api-1rq3.onrender.com'),
         'secret' => env('USERS1_SERVICE_SECRET'),
     ],
 
     'users2' => [
-        'base_uri' => env('USER2_SERVICE_BASE_URL', 'http://localhost:8001/'),
+        'base_uri' => env('USERS2_SERVICE_BASE_URL', 'https://ddsbe2-zdn1.onrender.com'),
         'secret' => env('USERS2_SERVICE_SECRET'),
     ],
 ];
